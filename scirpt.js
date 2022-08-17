1d3171e96a2127f99a6166f1befadea3c32f6107
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-17 11:48:16 +0530

    full ulTag with child created

--- scirpt.js
+++ scirpt.js
@@ -7,7 +7,7 @@ var deleteAllBtn = document.querySelector('.deleteBtn');
 
 addingButton.addEventListener('click',function(e){
 
-     if(Enput.ariaValueMax.trim())
+     if(Enput.value.trim())
      {
         var ulTag = document.createElement('ul');
         ulTag.classList.add('todo-list-container');
@@ -17,7 +17,8 @@ addingButton.addEventListener('click',function(e){
         todoList.classList.add('todo-list');
 
         var liTag = document.createElement('li');
-        liTag.innerHTML = input.value;
+        liTag.innerHTML = Enput.value;
+        liTag.classList.add('todo-item');
 
         var buttonDiv  = document.createElement('div');
         buttonDiv.classList.add('button');
@@ -25,6 +26,26 @@ addingButton.addEventListener('click',function(e){
         var completeButton = document.createElement('button');
         completeButton.classList.add('completed');
         completeButton.innerHTML = ' <i class = "fa fa-solid fa-check"></i>'
+
+        
+        var editButton = document.createElement('button');
+        completeButton.classList.add('editBtn');
+        completeButton.innerHTML = '    <i class = "fa fa-plus fa-add-item"></i>'
+
+        var trashButton = document.createElement('button');
+        completeButton.classList.add('trash');
+        completeButton.innerHTML = '<i class="fa fa-solid fa-edit" ></i>'
+
+
+        ulTag.appendChild(todoList);
+        todoList.appendChild(liTag);
+        todoList.appendChild(buttonDiv);
+        buttonDiv.appendChild(completeButton);
+        buttonDiv.appendChild(editButton);
+        buttonDiv.appendChild(trashButton);
+        console.log(ulTag);
+
+
 
     }
 
