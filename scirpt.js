17dc32b57cfe2eee2247550f4dabdf81f9fecb9f
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-17 10:17:26 +0530

    js init

---
+++ scirpt.js
@@ -0,0 +1,5 @@
+var MainTodoContainer = document.getElementById('todos');
+var input =  document.querySelector('.todo_input');
+var addingButton = document.querySelector('.add-item');
+var deleteAllBtn = document.querySelector('.deleteBtn');
+
